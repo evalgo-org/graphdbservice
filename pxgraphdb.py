2d7881955b6b46babc37a9692f02b2fff601fc95
franciscsimon <francisc.simon@pantopix.com>
2024-11-08 12:56:37 +0000

    Update pxgraphdb.py

--- pxgraphdb.py
+++ pxgraphdb.py
@@ -72,7 +72,7 @@ def exists_repository(server: str, name: str, username: str = '', password: str 
         basic = HTTPBasicAuth(username, password)
         resp = requests.get(server, auth=basic)
     else:
-        resp = requests.get('http://ke1.graphdb.px:7200/rest/repositories')
+        resp = requests.get(server)
     found = list(filter(lambda r: True if r['id'] == name else False, resp.json()))
     if len(found) > 0:
         return True
