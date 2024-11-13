8c217d05d4705da7dae5600eab7818b9bf81af61
franciscsimon <francisc.simon@pantopix.com>
2024-11-13 20:54:51 +0100

    updated pxgraphdb

--- pxknowledge_graph/pxgraphdb.py
+++ pxknowledge_graph/pxgraphdb.py
@@ -137,7 +137,7 @@ def filter_error_response(resp: dict, export_responses: list[dict]):
 @flow(log_prints=True)
 def export_import_repos_graphs(src_url: str, prefix: str, src_repo: str, graphs: list[str], tgt_url: str, tgt_repo: str, src_user: str = '', src_passwd: str = '', tgt_user: str = '', tgt_passwd: str = ''):
     export_responses = list(map(lambda g: delayed_graph_export(src_url, prefix, src_repo, g, src_user, src_passwd), graphs))
-    list(map(lambda g: print("exported graph", g), resp_graphs))
+    list(map(lambda g: print("exported graph", g), export_responses))
     import_responses = list(map(lambda g: {'graph': g['graph'], 'response': delayed_graph_import(tgt_url, tgt_repo, g['graph'], g['file'], tgt_user, tgt_passwd)}, export_responses))
     # error_responses = list(filter(lambda resp: True if resp['response'].status_code == 400 else False, import_responses))
     # if len(error_responses) > 0:
