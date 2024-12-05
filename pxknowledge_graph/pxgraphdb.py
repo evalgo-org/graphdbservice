c3756b0c5b91ba983e2f35262f543879973ade08
franciscsimon <francisc.simon@pantopix.com>
2024-12-05 11:26:23 +0100

    added more pytests to repo

--- pxknowledge_graph/pxgraphdb.py
+++ pxknowledge_graph/pxgraphdb.py
@@ -6,8 +6,8 @@ from requests.auth import HTTPBasicAuth
 from jinja2 import Template
 
 from pxinfra.pxdocker import PXDocker
-from pxinfra import pxbackup
-from pxinfra import pxrestore
+from pxinfra.pxbackup import PXBackupGraphDB
+from pxinfra.pxrestore import PXRestoreGraphDB
 from pxinfra.pxexport import PXExportGraphDB
 from pxinfra.pximport import PXImportGraphDB
 from pxinfra.pxinfisical import PXInfisical
@@ -24,6 +24,8 @@ class PXGraphDB:
         self.pxd = PXDocker(docker_host, docker_api_host)
         self.exp = PXExportGraphDB(exp_url, exp_user, exp_pass)
         self.imp = PXImportGraphDB(imp_url, imp_user, imp_pass)
+        self.bkp = PXBackupGraphDB(exp_url, exp_user, exp_pass)
+        self.res = PXRestoreGraphDB(imp_url, imp_user, imp_pass)
     def load_imp(self, url: str , user: str = '', passwd: str = ''):
         self.imp.url = url
         self.imp.username = user
@@ -34,6 +36,14 @@ class PXGraphDB:
         self.exp.username = user
         self.exp.password = passwd
         self.exp_repos = self.exp.graphdb_repositories()
+    def load_res(self, url: str , user: str = '', passwd: str = ''):
+        self.res.url = url
+        self.res.username = user
+        self.res.password = passwd
+    def load_bkp(self, url: str , user: str = '', passwd: str = ''):
+        self.bkp.url = url
+        self.bkp.username = user
+        self.bkp.password = passwd
     @task(log_prints=True,persist_result=False)
     def default(self, name: str):    
         gdb_pull = self.pxd.image_pull(PX_GRAPHDB_IMAGE, PX_GRAPHDB_VERSION)
@@ -61,8 +71,10 @@ class PXGraphDB:
         found_cnt = self.pxd.container_by_name(name)
         if found_cnt is not None:
             if rebuild:
+                resp_dict['rebuild'] = rebuild
                 self.pxd.container_remove(name)
             else:
+                resp_dict['rebuild'] = False
                 resp_dict['container'] = found_cnt
                 return resp_dict
         gdb_pull = self.pxd.image_pull(PX_GRAPHDB_IMAGE, PX_GRAPHDB_VERSION)
@@ -76,6 +88,7 @@ class PXGraphDB:
         container = self.pxd.container_run_ports(':'.join([PX_GRAPHDB_IMAGE, str(PX_GRAPHDB_VERSION)]), name, gdb_nw['name'], mounts, ports, {})
         # todo: check container
         resp_dict['container'] = container
+        resp_dict['rebuild'] = True
         return resp_dict
     @task(log_prints=True,persist_result=False)
     def default_remove(self, name: str):
@@ -85,69 +98,31 @@ class PXGraphDB:
         removed = container['container'].remove()
         # todo: check removed
         return removed
-    @task(log_prints=True,persist_result=False)
-    def create_repository(self, name: str):
-        if not exists_repository(server, name, username, password):
-            tmpl_content = None
-            tmpl_file = name+'.ttl'
-            with open(path.dirname(__file__)+path.sep+'new_repository_config.ttl.jinja') as tmpl:
-                tmpl_content = Template(tmpl.read())
-            with open(tmpl_file, 'w') as wtmpl:
-                wtmpl.write(tmpl_content.render(repository_id=name))
-            files = {'config':open(tmpl_file)}
-            if username != '' and password != '':
-                basic = HTTPBasicAuth(username, password)
-                return requests.post(server, files=files, auth=basic)
-            return requests.post(server+'/rest/repositories', files=files)
-        return None
-    @task(log_prints=True,persist_result=False)
-    def delete_repository(self, server: str, name: str, username: str = '', password: str = ''):
-        if exists_repository(server, name, username, password):
-            return requests.delete(server+'/rest/repositories/'+name)
-        return None
     @flow(log_prints=True,persist_result=False)
-    def backup_restore(self, src_url: str, prefix: str, repos: list[str], src_user:str = '', src_passwd: str = '', tgt_url: str = '', tgt_user: str = '', tgt_passwd: str = ''):
-        bkup = pxbackup.graphdb(src_url, prefix, repos, src_user, src_passwd)
-        if tgt_user != '' and tgt_passwd != '':
-            return pxrestore.graphdb(tgt_url, bkup, username=tgt_user, password=tgt_passwd)
-        return pxrestore.graphdb(tgt_url, bkup)
+    def backup_restore(self, prefix: str, repos: list[str]):
+        bkup = self.bkp.graphdb(prefix, repos)
+        return self.res.graphdb(bkup)
     @flow(log_prints=True,persist_result=False)
-    def export_import_repos(self, src_url: str, prefix: str, repos: list[str], src_user: str = '', src_passwd: str = '', tgt_url: str = '', tgt_user: str = '', tgt_passwd: str = ''):
-        px_exp = PXExportGraphDB(src_url, src_user, src_passwd)
-        resp_repos = list(map(lambda r: {'repo': r, 'files': px_exp.graphdb_repo(prefix=prefix, repo=r)}, repos))
-        px_imp = PXImportGraphDB(tgt_url, tgt_user, tgt_passwd)
-        resp_import = list(map(lambda r: px_imp.graphdb_repo_api(r['repo'], r['files']['data'], r['files']['conf']), resp_repos))
+    def export_import_repos(self, prefix:str, repos: list[str]):
+        resp_repos = list(map(lambda r: {'repo': r, 'files': self.exp.graphdb_repo(prefix=prefix, repo=r)}, repos))
+        resp_import = list(map(lambda r: self.imp.graphdb_repo_api(r['repo'], r['files']['data'], r['files']['conf']), resp_repos))
         return resp_import
-    @flow(log_prints=True,persist_result=False)
-    def delayed_graph_export(self, url: str, prefix: str, repo: str, graph: str, username: str, password:str):
-        time.sleep(5)
-        px_exp = PXExportGraphDB(url, username, password)
-        return px_exp.graphdb_repo_graph(prefix, repo, graph)
-    @flow(log_prints=True,persist_result=False)
-    def delayed_graph_import(self, url: str, repo: str, graph: str, graph_file: str, username: str, password:str):
-        time.sleep(5)
-        px_imp = PXImportGraphDB(url, username, password)
-        return px_imp.graphdb_graph(repo, graph, graph_file)
-    def filter_error_response(resp: dict, export_responses: list[dict]):
+    def filter_error_response(self, resp: dict, export_responses: list[dict]):
         found_errors = list(filter(lambda graph: True if graph['graph'] == resp['graph'] else False, export_responses))
         if len(found_errors) > 0:
             return found_errors[0]
         return None
     @task(log_prints=True,persist_result=False)
-    def graph_import_with_check(self, url: str, repo: str, graph: str, graph_file:str, username: str, passwd: str):
-        time.sleep(2)
-        px_imp = PXImportGraphDB(url, username, passwd)
-        imp_resp = px_imp.graphdb_graph(repo, graph, graph_file)
-        px_exp = PXExportGraphDB(url, username, passwd)
-        if not px_exp.graphdb_graph_exists(repo, graph):
+    def graph_import_with_check(self, repo: str, graph: str, graph_file:str):
+        imp_resp = self.imp.graphdb_graph(repo, graph, graph_file)
+        if not self.exp.graphdb_graph_exists(repo, graph):
             print(f"reimport {url}::{repo} graph {graph} from file {graph_file}...")
-            time.sleep(2)
-            return px_imp.graphdb_graph(repo, graph, graph_file)
+            return self.imp.graphdb_graph(repo, graph, graph_file)
     @flow(log_prints=True,persist_result=False)
-    def export_import_repos_graphs(self, src_url: str, prefix: str, src_repo: str, graphs: list[str], tgt_url: str, tgt_repo: str, src_user: str = '', src_passwd: str = '', tgt_user: str = '', tgt_passwd: str = ''):
-        export_responses = list(map(lambda g: delayed_graph_export(src_url, prefix, src_repo, g, src_user, src_passwd), graphs))
+    def export_import_repos_graphs(self, prefix: str, src_repo: str, graphs: list[str], tgt_repo: str):
+        export_responses = list(map(lambda g: self.exp.graphdb_repo_graph(prefix, src_repo, g), graphs))
         list(map(lambda g: print("exported graph", g), export_responses))
-        import_responses = list(map(lambda g: {'graph': g['graph'], 'response': graph_import_with_check(tgt_url, tgt_repo, g['graph'], g['file'], tgt_user, tgt_passwd)}, export_responses))
+        import_responses = list(map(lambda g: {'graph': g['graph'], 'response': self.graph_import_with_check(tgt_repo, g['graph'], g['file'])}, export_responses))
         # error_responses = list(filter(lambda resp: True if resp['response'].status_code == 400 else False, import_responses))
         # if len(error_responses) > 0:
         #     retry_err_responses = list(filter(lambda resp: filter_error_response(resp, export_responses), ))
---
+++ tests/test_local.py
@@ -0,0 +1,64 @@
+import pytest
+import time
+
+def test_backup_restore(settings):
+    cnt = 'test-local-backup-restore'
+    created = settings.pxgraphdb.default_ports(cnt, {settings.settings['RST_SRV_PORT']:settings.settings['RST_SRV_PORT']})
+    if created['rebuild']:
+        time.sleep(30)
+    settings.pxgraphdb.load_bkp(
+        settings.settings['BKP_SRV'],
+        settings.settings['BKP_USER'],
+        settings.settings['BKP_PASS']
+    )
+    settings.pxgraphdb.load_res(
+        settings.settings['RST_SRV']+':'+settings.settings['RST_SRV_PORT']
+    )
+    repos = [
+        'ProductData-KKH-EG-US',
+        'ProductData-KKH-keys-EG',
+        'ProductData-KKH-keys-US',
+        'ProductData-MDM-keys-EG',
+        'ProductData-MDM-keys-US',
+        'ProductData-KKH-keys-EG',
+        'ProductData-MDM-EG-US']
+    settings.pxgraphdb.backup_restore("c5-to-local", repos)
+    settings.pxgraphdb.default_remove(cnt)
+
+def test_export_import_repos(settings):
+    cnt = 'test-local-import-export'
+    created = settings.pxgraphdb.default_ports(cnt, {settings.settings['RST_SRV_PORT']:settings.settings['RST_SRV_PORT']})
+    if created['rebuild']:
+        time.sleep(30)
+    settings.pxgraphdb.load_exp(
+        settings.settings['BKP_SRV'],
+        settings.settings['BKP_USER'],
+        settings.settings['BKP_PASS']
+    )
+    settings.pxgraphdb.load_imp(
+        settings.settings['RST_SRV']+':'+settings.settings['RST_SRV_PORT']
+    )
+    repos = ['Chatbot-Demo', 'dataCatalog']
+    settings.pxgraphdb.export_import_repos("c5-to-local-import", repos)
+    settings.pxgraphdb.default_remove(cnt)
+
+def test_export_import_repos_graphs(settings):
+    cnt = 'test-local-import-export-graphs'
+    created = settings.pxgraphdb.default_ports(cnt, {settings.settings['RST_SRV_PORT']:settings.settings['RST_SRV_PORT']})
+    if created['rebuild']:
+        time.sleep(30)
+    settings.pxgraphdb.load_exp(
+        settings.settings['BKP_SRV'],
+        settings.settings['BKP_USER'],
+        settings.settings['BKP_PASS']
+    )
+    settings.pxgraphdb.load_imp(
+        settings.settings['RST_SRV']+':'+settings.settings['RST_SRV_PORT']
+    )
+    settings.pxgraphdb.imp.
+    settings.pxgraphdb.export_import_repos_graphs(
+        "c5-to-local-import-graphs", 
+        'Chatbot-Demo', 
+        ['https://data.kaeser.com/KKH/MDM','https://data.kaeser.com/KKH/MDM/PH'],
+        'Chatbot-Demo')
+    settings.pxgraphdb.default_remove(cnt)
