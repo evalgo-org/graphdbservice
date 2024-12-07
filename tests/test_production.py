3af3a6375d08a91d007df6902e0625111fd9bce8
franciscsimon <francisc.simon@pantopix.com>
2024-12-07 23:48:41 +0100

    removed legacy and added the important prod tests

--- tests/test_production.py
+++ tests/test_production.py
@@ -42,4 +42,33 @@ def test_export_import_c5_prod(settings):
         'ProductData-MDM-keys-US',
         'ProductData-KKH-keys-EG',
         'ProductData-MDM-EG-US']
-    settings.pxgraphdb.imp.graphdb_repo_api('ProductData-KKH-keys-EG',exp_info['data'], exp_info['conf'])
+    for repo in repos:
+        settings.pxgraphdb.imp.graphdb_repo_api(repo, exp_info['data'], exp_info['conf'])
+
+def test_export_import_maintenance_from_dev_to_prod(settings):
+    settings.pxgraphdb.load_exp(
+        'http://dev.graphdb.px:7200',
+        '',
+        ''
+    )
+    exp_info = settings.pxgraphdb.exp.graphdb_repo('dev','Maintenance-EG')
+    settings.pxgraphdb.load_imp(
+        settings.settings['KAESER_GRAPHDB_PROD_URL'],
+        settings.settings['KAESER_GRAPHDB_PROD_USER'],
+        settings.settings['KAESER_GRAPHDB_PROD_PASS']
+    )
+    settings.pxgraphdb.imp.graphdb_repo_api('Maintenance-EG',exp_info['data'], exp_info['conf'])
+
+def test_export_import_consumption_nav_from_dev_to_prod(settings):
+    settings.pxgraphdb.load_exp(
+        'http://dev.graphdb.px:7200',
+        '',
+        ''
+    )
+    exp_info = settings.pxgraphdb.exp.graphdb_repo('dev','Consumption-Navigator-DE-Intern-All')
+    settings.pxgraphdb.load_imp(
+        settings.settings['KAESER_GRAPHDB_PROD_URL'],
+        settings.settings['KAESER_GRAPHDB_PROD_USER'],
+        settings.settings['KAESER_GRAPHDB_PROD_PASS']
+    )
+    settings.pxgraphdb.imp.graphdb_repo_api('Consumption-Navigator-DE-Intern-All',exp_info['data'], exp_info['conf'])
