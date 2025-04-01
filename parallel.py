7c33f67287f5f3bc8c741e32c0297b0de8120a4f
franciscsimon <francisc@simon.services>
2025-04-01 13:24:02 +0200

    bumped pxinfra version

--- parallel.py
+++ parallel.py
@@ -1,7 +1,20 @@
 from operator import add
-from dask.distributed import Client
+from prefect_dask import DaskTaskRunner, get_dask_client
+from prefect import flow, task
+import time
 
-client = Client('172.20.0.2:8786')
-x = client.submit(add, 1, 2)
-result = client.gather(x)
-print(result)
+@task(log_prints=True)
+def added(x, y):
+    time.sleep(5)
+    print(add(x,y))
+
+@flow(task_runner=DaskTaskRunner(address="172.20.0.3:8786"), log_prints=True)
+def test_dask_flow():
+    futures = []
+    for i in range(0,100):
+        futures.append(added.submit(i, 5))
+    print(futures)
+    list(map(lambda f: print(f.result()), futures))
+
+if __name__ == '__main__':
+    test_dask_flow()
